fdba293063146446c0b71e385b9c0d8534199651
c0deslinger <yusuf.fachroni@gmail.com>
2025-11-11 15:29:34 +0700

    add frame

---
+++ frame.php
@@ -0,0 +1,7 @@
+<!--
+Source - https://stackoverflow.com/a/66876917
+Posted by Dale
+Retrieved 2025-11-11, License - CC BY-SA 4.0
+-->
+
+<!DOCTYPE html><html><?php echo file_get_contents($_REQUEST['url']); ?></html>
